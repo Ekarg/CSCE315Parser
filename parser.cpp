dfee93e360d3f59966581b5eb82758cddee6e4ac
Dapeebb <wesleytang123@gmail.com>
2013-09-12 10:30:48 -0500

    Things happened.
    -Did insert. Again, since we aren't integrating it, there isn't too much
    I can do other than check if the command is valid. Can't check if
    inserted entity is valid within table; can't check if expression is
    valid, etc.

--- parser.cpp
+++ parser.cpp
@@ -67,14 +67,67 @@ bool Parser::validate(string input) {
 	}
 	if(exit.compare(first) == 0)
 	{
-		// WES TANG
 		printf("Exit command parsed.\n\n");
 		// exit();
 		return true; 
 	}
-	if(insert.compare(first) == 0)
+	if( (insert.compare(first) == 0) && (tokens[1] == "INTO") && (tokens[3] == "VALUES") && (tokens[4] == "FROM") && (tokens.size() > 5) )
 	{
-		// WES TANG
+		string rel_name = tokens[2];
+		tokens.erase(tokens.begin(), tokens.begin()+5);
+		// Case for "INSERT INTO points VALUES FROM RELATION (select (z2 > 0) dots_to_points);"
+		if (tokens[0] == "RELATION") {
+			tokens.erase(tokens.begin());
+			string expr;
+			for (int i = 0; i < tokens.size(); ++i) {
+				expr += tokens[i];
+			}
+			if (expr.back() == ';') {
+				expr.pop_back();
+			}
+			else {
+				printf("Missing Semicolon.\n");
+				return false;
+			}
+			cout << "expr == " << expr << endl;
+			//parse_expr(expr);
+			//union(table1, table2);
+		}
+		// Case for "INSERT INTO dots VALUES FROM (0, 0, 0);"
+		else {
+			string expr;
+			for (int i = 0; i < tokens.size(); ++i) {
+				expr += tokens[i];
+			}
+			if (expr.back() == ';') {
+				expr.pop_back();
+			}
+			else {
+				printf("Missing Semicolon.\n");
+				return false;
+			}
+			vector<string> args;
+			for (int j = 0; j < expr.size(); ++j) {
+				if (expr[j] == '(') {
+					continue;
+				}
+				if (expr[j] == '"') {
+					continue;
+				}
+				string a;
+				while ( (expr[j] != ',') && (j < expr.size()) && (expr[j] != ')') ) {
+					a += expr[j];
+					++j;
+				}
+				args.push_back(a);
+			}
+			for (int k = 0; k < tokens.size(); ++k) {
+				cout << args[k] << endl;
+			}
+			//create entity with values from args
+			//check if entity is allowable into table rel_name
+			//insert entity into tablr rel_name
+		}
 		return true; 
 	}
 	if( (del.compare(first) == 0) && (tokens[1] == "FROM") && (tokens[3] == "WHERE") && (tokens.size() > 4) )
